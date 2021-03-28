a77e35625af843b2b514569af34c4bb1b4d7fdeb
Manash Bhaduri <bhaduri.maharshi@gmail.com>
2021-03-29 01:11:48 +0530

    animation bug fix

--- js/app.js
+++ js/app.js
@@ -1,27 +1,30 @@
 $(".card-1").on("mouseenter", function() {
-    $(".card-1").animate({
+    $(".card-1").stop(true, false).animate({
         height: "210px",
         width: "610px"
     })
 });
 
 $(".card-1").on("mouseleave", function() {
-    $(".card-1").animate({
+    $(".card-1").stop(true, false).animate({
         height: "200px",
         width: "600px"
     })
 });
 
 $(".card-logo").on("mouseenter", function() {
-    $(".card-logo").stop(false, true).animate({
-        height: "+=42.5px"
-    });
-    $(".about").stop(false, true).delay(300).fadeToggle();
+    $(".card-logo").stop(true, true).animate({
+        height: "95.5px"
+    }, 400, function() {
+        $(".about").delay(300).fadeToggle();
+    })
 });
 
 $(".card-logo").on("mouseleave", function() {
-    $(".about").stop(false, true).delay(700).fadeToggle();
-    $(".card-logo").stop(false, true).delay(1000).animate({
-        height: "-=42.5px"
+    $(".about").stop(true, true).delay(700).fadeToggle(function() {
+        $(".card-logo").delay(1000).animate({
+            height: "52px"
+        });
     });
+
 });
